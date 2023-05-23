d67e26be882ade9ea8f49d38c452869047fcb4c0
Preshgee <okorepreciousu@gmail.com>
2023-05-23 15:22:07 -0700

    Function that frees new dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to structure
+ *
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
