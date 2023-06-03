3e07084068bfdb05ef9393fc2997dc4766db9787
Preshgee <okorepreciousu@gmail.com>
2023-06-03 04:05:55 -0700

    my goal

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
- * _islower - Checks for lowercase character
- * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
+
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
-/**
- * _abs - Prints the absolute value of an integer.
- * @a: The integer to compute its absolute value.
- * Return: Returns the abs value.
- */
-int _abs(int a)
-{
-	int k = a;
-	int absvalue;
 
-	if (k < 0)
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
 	{
-		k = k * (-1);
+		return (n);
 	}
-	absvalue = k;
-	return (absvalue);
+	return (-n);
 }
+
