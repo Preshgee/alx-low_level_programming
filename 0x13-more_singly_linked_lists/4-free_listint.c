488c791dd7b9204329cf06106f2a2902fa4c3b1a
Preshgee <okorepreciousu@gmail.com>
2023-06-06 13:39:24 -0700

    A function that frees linked list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,20 +5,19 @@
 /**
  * free_listint - frees a list_t list
  * @head: first element of the list
- *
  * Return: no return
  */
 void free_listint(listint_t *head)
 {
-	struct listint_s *tmp = NULL;
+	struct listint_s *temp = NULL;
 	struct listint_s *aux =  NULL;
 
-	tmp = head;
-	while (tmp != NULL)
+	temp = head;
+	while (temp != NULL)
 	{
-		aux = tmp->next;
-		free(tmp);
-		tmp = aux;
+		aux = temp->next;
+		free(temp);
+		temp = aux;
 	}
 	head = NULL;
 }
